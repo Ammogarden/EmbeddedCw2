75163ef8b0ef8d2d408a94534ed678531b769f06
Ammogarden <jdx16@ic.ac.uk>
2019-03-07 09:21:01 +0000

    CW2 started

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ State   L1  L2  L3
 const int8_t driveTable[] = {0x12,0x18,0x09,0x21,0x24,0x06,0x00,0x00};
 
 //Mapping from interrupter inputs to sequential rotor states. 0x00 and 0x07 are not valid
-const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};  
+const int8_t stateMap[] = {0x07,0x05,0x03,0x04,0x01,0x00,0x02,0x07};
 //const int8_t stateMap[] = {0x07,0x01,0x03,0x02,0x05,0x00,0x04,0x07}; //Alternative if phase order of input or drive is reversed
 
 //Phase lead to make motor spin
@@ -65,12 +65,44 @@ DigitalOut L2H(L2Hpin);
 DigitalOut L3L(L3Lpin);
 DigitalOut L3H(L3Hpin);
 
+//Timer
+Timer timer;
+
+//Mail
+typedef struct {
+    uint64_t result;
+} mail_t;
+
+Mail<mail_t, 16> mail_box;
+
+void putMessage(uint64_t result){
+    mail_t *mail = mail_box.alloc();
+    mail->result = result;
+    mail_box.put(mail);
+}
+
+//Threads
+Thread out_thread;
+
+void sendSerial(){
+    Serial pc(SERIAL_TX, SERIAL_RX);
+    while(true){
+        osEvent evt = mail_box.get();
+        if(evt.status == osEventMail){
+            mail_t *mail = (mail_t*)evt.value.p;
+            pc.printf("Nonce found:");
+            pc.printf("%016llX\n\r", mail->result);
+            mail_box.free(mail);
+        }
+    }
+}
+
 //Set a given drive state
 void motorOut(int8_t driveState){
-    
+
     //Lookup the output byte from the drive state.
     int8_t driveOut = driveTable[driveState & 0x07];
-      
+
     //Turn off first
     if (~driveOut & 0x01) L1L = 0;
     if (~driveOut & 0x02) L1H = 1;
@@ -78,7 +110,7 @@ void motorOut(int8_t driveState){
     if (~driveOut & 0x08) L2H = 1;
     if (~driveOut & 0x10) L3L = 0;
     if (~driveOut & 0x20) L3H = 1;
-    
+
     //Then turn on
     if (driveOut & 0x01) L1L = 1;
     if (driveOut & 0x02) L1H = 0;
@@ -87,18 +119,18 @@ void motorOut(int8_t driveState){
     if (driveOut & 0x10) L3L = 1;
     if (driveOut & 0x20) L3H = 0;
     }
-    
+
     //Convert photointerrupter inputs to a rotor state
 inline int8_t readRotorState(){
     return stateMap[I1 + 2*I2 + 4*I3];
     }
 
-//Basic synchronisation routine    
+//Basic synchronisation routine
 int8_t motorHome() {
     //Put the motor in drive state 0 and wait for it to stabilise
     motorOut(0);
     wait(2.0);
-    
+
     //Get the rotor state
     return readRotorState();
 }
@@ -108,19 +140,27 @@ void ISR_turn(){//check current state when the state changes and then drive moto
         intStateOld = intState;
         motorOut((intState-orState+lead+6)%6); //+6 to make sure the remainder is positive
         //pc.printf("%d\n\r",intState);
-    }    
+    }
 }
 
 
-    
+
 //Main
 int main() {
     //Initialise the serial port
-    Serial pc(SERIAL_TX, SERIAL_RX);
-    pc.printf("Hello\n\r");
+    RawSerial pc(SERIAL_TX, SERIAL_RX);  //TODO: check: should i still establish serial connection in main() given that there is a thread meant to use the serial port?
+    pc.baud(9600);
+    //pc.printf("Hello\n\r");
+
+    out_thread.start(sendSerial);
+
+
+
+
+    //
     //Run the motor synchronisation
     orState = motorHome();
-    pc.printf("Rotor origin: %x\n\r",orState);
+    //pc.printf("Rotor origin: %x\n\r",orState);
     //orState is subtracted from future rotor state inputs to align rotor and motor states
     //attach ISR to each pin's rising and falling edge
     I1.rise(&ISR_turn);
@@ -131,9 +171,34 @@ int main() {
     I3.fall(&ISR_turn);
     //declare SHA256 instance
     SHA256 h;
-    //Poll the rotor state and set the motor outputs accordingly to spin the motor
-    while (1) {
+    //256 bits are 64 bytes
+    uint8_t sequence[] = {0x45,0x6D,0x62,0x65,0x64,0x64,0x65,0x64,
+                          0x20,0x53,0x79,0x73,0x74,0x65,0x6D,0x73,
+                          0x20,0x61,0x72,0x65,0x20,0x66,0x75,0x6E,
+                          0x20,0x61,0x6E,0x64,0x20,0x64,0x6F,0x20,
+                          0x61,0x77,0x65,0x73,0x6F,0x6D,0x65,0x20,
+                          0x74,0x68,0x69,0x6E,0x67,0x73,0x21,0x20,
+                          0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
+                          0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
 
+    uint64_t* key = (uint64_t*)((int)sequence + 48);
+    uint64_t* nonce = (uint64_t*)((int)sequence + 56);
+    uint8_t hash[32];
+
+    //Poll the rotor state and set the motor outputs accordingly to spin the motor
+    int i = 0;
+    timer.start();
+    while (1) {
+        (*nonce)++;
+        h.computeHash(hash, sequence, 64);
+        i++;
+        if(hash[0] == hash[1] && hash[0] == 0)
+            putMessage(*nonce);
+            //pc.printf("%016llX\n", *nonce);
+        if(timer.read() > 1){
+            pc.printf("hash count:%d\n\r",i);
+            i = 0;
+            timer.reset();
+        }
     }
 }
-
